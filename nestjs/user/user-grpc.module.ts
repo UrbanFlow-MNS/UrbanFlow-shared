b8dad9452693edfb36287b5e54a4641337dd99c7
Theo Sementa <theosementa@gmail.com>
2026-07-05 09:31:34 +0200

    feat: add shared module

---
+++ nestjs/user/user-grpc.module.ts
@@ -0,0 +1,25 @@
+import { Module } from "@nestjs/common";
+import { ClientsModule, Transport } from "@nestjs/microservices";
+import { join } from "path";
+
+@Module({
+    imports: [
+        ClientsModule.register([
+            {
+                name: "USER_PACKAGE",
+                transport: Transport.GRPC,
+                options: {
+                    package: "user",
+                    protoPath: join(
+                        __dirname,
+                        "../../../proto/files/user.proto",
+                    ),
+                    url: process.env.USER_SERVICE_GRPC_URL ?? "localhost:7006",
+                    loader: { enums: String },
+                },
+            },
+        ]),
+    ],
+    exports: [ClientsModule],
+})
+export class UserGrpcModule {}
